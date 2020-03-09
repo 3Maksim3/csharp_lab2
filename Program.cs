d802740a4505a36b6bc9a2a69640a9edabe6cd85
alexsudochakov37@gmail.com <alexsudochakov37@gmail.com>
2020-03-09 20:37:38 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharp_lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number and press enter: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number and press enter: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("result=" + Raz(a, b));
+        }
+        public static int Raz(int a, int b)
+        {
+            return (a - b);
+        }
+
+    }
+}
